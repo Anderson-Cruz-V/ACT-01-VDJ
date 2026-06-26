362efabcf5a6858b8c8bd725a1f79db575712575
alexis0314 <alexismercedes365@gmail.com>
2026-06-25 23:47:53 -0400

    Alexis

--- Assets/shuriken.cs
+++ Assets/shuriken.cs
@@ -2,10 +2,27 @@ using UnityEngine;
 
 public class Shuriken : MonoBehaviour
 {
-    private void OnTriggerEnter2D(Collider2D collision)
+    [SerializeField] private GameObject efecto; 
+    [SerializeField] private float cantidadPuntos = 100f; 
+    [SerializeField] private Puntaje puntaje; 
+
+    private void OnTriggerEnter2D(Collider2D other)
     {
-        if (collision.CompareTag("Player"))
+        if (other.CompareTag("Player"))
         {
+            
+            if (puntaje != null)
+            {
+                puntaje.SumarPuntos(cantidadPuntos);
+            }
+
+            
+            if (efecto != null)
+            {
+                Instantiate(efecto, transform.position, Quaternion.identity);
+            }
+
+            
             Destroy(gameObject);
         }
     }
