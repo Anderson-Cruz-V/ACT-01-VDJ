1f4dcdc6dea015569d2fcd0525951098eb52cbb5
Anderson-Cruz-V <213361341+Anderson-Cruz-V@users.noreply.github.com>
2026-07-03 00:47:20 -0400

    EnemyListo

---
+++ Assets/EnemyController.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+
+public class EnemyController:MonoBehaviour
+{
+    public Transform player;
+    public float detectionRadius = 5.0f;
+    public float speed = 2.0f;
+
+    private Rigidbody2D rd;
+    private Vector2 movement;
+    void Start()
+    {
+        rd = GetComponent<Rigidbody2D>();
+
+    }
+
+    void Update()
+    {
+        float distaceTopayer =  Vector2.Distance( transform.position, player.position );
+        if (distaceTopayer < detectionRadius)
+        {
+            Vector2 direction = (player.position - transform.position).normalized;
+            
+            movement = new Vector2 (direction.x,0);
+        }
+
+        else
+        {
+            movement = Vector2.zero;
+        }
+        rd.MovePosition(rd.position+ movement*speed*Time.deltaTime);
+    }
+
+}
+    
