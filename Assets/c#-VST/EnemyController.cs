ef1ee8887c96b2e7b60b292b206f0a6b334e626c
Anderson-Cruz-V <213361341+Anderson-Cruz-V@users.noreply.github.com>
2026-07-10 19:39:18 -0400

    EDle ,caminar

--- Assets/c#-VST/EnemyController.cs
+++ Assets/c#-VST/EnemyController.cs
@@ -1,9 +1,6 @@
-using System.Collections;
-using System.Collections.Generic;
-
 using UnityEngine;
 
-public class EnemyController:MonoBehaviour
+public class EnemyController : MonoBehaviour
 {
     public Transform player;
     public float detectionRadius = 5.0f;
@@ -11,28 +8,46 @@ public class EnemyController:MonoBehaviour
 
     private Rigidbody2D rd;
     private Vector2 movement;
+    private SpriteRenderer spriteRenderer;
+    private Animator animator;
+
     void Start()
     {
         rd = GetComponent<Rigidbody2D>();
-
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        animator = GetComponent<Animator>();
     }
 
     void Update()
     {
-        float distaceTopayer =  Vector2.Distance( transform.position, player.position );
-        if (distaceTopayer < detectionRadius)
+        float distanceToPlayer = Vector2.Distance(transform.position, player.position);
+
+        if (distanceToPlayer < detectionRadius)
         {
             Vector2 direction = (player.position - transform.position).normalized;
-            
-            movement = new Vector2 (direction.x,0);
-        }
 
+            movement = new Vector2(direction.x, 0);
+
+            animator.SetBool("Caminando", true);
+
+            if (direction.x > 0)
+            {
+                spriteRenderer.flipX = false;
+            }
+            else if (direction.x < 0)
+            {
+                spriteRenderer.flipX = true;
+            }
+        }
         else
         {
             movement = Vector2.zero;
+            animator.SetBool("Caminando", false);
         }
-        rd.MovePosition(rd.position+ movement*speed*Time.deltaTime);
     }
 
+    void FixedUpdate()
+    {
+        rd.MovePosition(rd.position + movement * speed * Time.fixedDeltaTime);
+    }
 }
-    
